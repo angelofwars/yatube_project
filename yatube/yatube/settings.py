a514f5937f275960ec26be54d710a7750819b235
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-02 15:03:33 +0300

    ДобваиÐл виртуальноеÐ пространстÐво с (py 3.7 and django 2.2.19)

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'post.apps.PostIceCream'
-    'ice_cream.apps.IceCreamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
