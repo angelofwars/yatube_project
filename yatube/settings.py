c2042e764df490b9f1bfdb23daddd5f9df85d1ca
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-09 11:59:33 +0300

    Не запускается серÐсайтÐ: приindex.html

--- yatube/settings.py
+++ yatube/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'yatube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/posts/index.html')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
