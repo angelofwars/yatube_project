7d2959cdeda690368aa54443deff3fa62fd14fe7
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-10 22:38:13 +0300

    ПÑсделал папки и пытался что то в них сделать

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    return render(request, 'posts/index.html')
 
 
 def posts_list(request):
-    return HttpResponse('Список постов')
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 
-def group_posts_list(request) :
-    return HttpResponse('Список групп')
+def group_posts_list(request, slug) :
+    return render(request, 'posts/group_list.html')
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
