c1f5d5538fb31c2de0ad312910d1d0d667225fb3
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-11 22:32:43 +0300

    Сделал сайт с поÐпомощью html нÐвроде бы есть ошибка

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,8 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='post_detail'),
+    path('posts', views.posts_list,
+         name='posts_list'),
     path(
         'group/<slug:slug>/',
         views.group_posts_detail,
--- posts/views.py
+++ posts/views.py
@@ -6,17 +6,15 @@ def index(request):
     return render(request, 'posts/index.html')
 
 
-def posts_list(request):
+def posts_list(request, slug):
     template = 'posts/group_list.html'
     return render(request, template)
 
 
-def group_posts_list(request, slug) :
-    return render(request, 'posts/group_list.html')
-
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts_detail(request, slug):
-    return HttpResponse(f'Группа постов {slug}')
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 
